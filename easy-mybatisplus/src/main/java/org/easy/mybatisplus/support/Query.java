d2be76ec1c053359863de3f550fac726df63b751
slh <635310229@qq.com>
2021-01-04 18:08:58 +0800

    Extjs page parames

--- easy-mybatisplus/src/main/java/org/easy/mybatisplus/support/Query.java
+++ easy-mybatisplus/src/main/java/org/easy/mybatisplus/support/Query.java
@@ -20,12 +20,24 @@ public class Query {
 	 */
 	@ApiModelProperty(value = "当前页")
 	private Integer current;
+	public Integer getCurrent(){
+		if(page!=null){
+			return page;
+		}
+		return current;
+	}
 
 	/**
 	 * 每页的数量
 	 */
 	@ApiModelProperty(value = "每页的数量")
 	private Integer size;
+	public Integer getSize(){
+		if(limit!=null){
+			return limit;
+		}
+		return size;
+	}
 
 	/**
 	 * 排序的字段名
@@ -38,5 +50,9 @@ public class Query {
 	 */
 	@ApiModelProperty(hidden = true)
 	private String descs;
+
+	private Integer page;//: 5
+	private Integer start;//: 40
+	private Integer limit;//: 10
 
 }
